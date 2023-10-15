904a18eaf15e28af22cbe0883335c0032ca4f298
Niyazi <berataktas2834@gmail.com>
2023-10-15 15:51:42 +0300

    3.gün ödev3

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    public class MusteriManager
+    {
+        //Naming convention
+        public void Ekle()
+        {
+            Console.WriteLine("Sepete eklendi");
+        }
+
+        public void Sil()
+        {
+            Console.WriteLine("Silme işlemi Başarılı");
+        }
+        public void listele()
+        {
+            Console.WriteLine("Müşteri listesi");
+
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassMetotDemo;
+
+//Console.WriteLine("Hello, World!");
+
+//string MusteriAdi = "Ahmet";
+//string MusteriSoyad = "Kılıç";
+//string Aciklama = "Deneme";
+
+
+Musteri musteri1 = new Musteri();
+musteri1.Adi = "Ali";
+musteri1.Soyadi = "Yılmaz";
+musteri1.aciklma = "Yeni müşteri";
+musteri1.Bakiye = 1000;
+
+Musteri musteri2 = new Musteri();
+musteri2.Adi = "Ayşe";
+musteri2.Soyadi = "Kara";
+musteri2.aciklma = "emekli";
+musteri2.Bakiye=5000;
+
+Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+foreach (Musteri musteri in musteriler)
+{
+    Console.WriteLine("Adı:" + musteri.Adi);
+    Console.WriteLine("Soyadı:" +musteri.Soyadi);
+    Console.WriteLine("Bakiye:" +musteri.Bakiye);
+    Console.WriteLine("------------------");
+
+}
+// instance - örnek
+MusteriManager musteriManager = new MusteriManager();
+musteriManager.Ekle();
+
+musteriManager.Sil();
+
+musteriManager.listele();
+musteriManager.listele();
+musteriManager.listele();
+musteriManager.listele();
+
+musteriManager.Ekle();
